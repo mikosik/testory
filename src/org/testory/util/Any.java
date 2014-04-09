eccbd565dbc1c388a5fb4d991a649413e2b0c294
Marcin Mikosik <marcin.mikosik@gmail.com>
2014-04-09 16:55:47 +0200

    refactored matching algorithm in Any

--- src/org/testory/util/Any.java
+++ src/org/testory/util/Any.java
@@ -2,16 +2,13 @@ package org.testory.util;
 
 import static java.util.Collections.nCopies;
 import static org.testory.common.CharSequences.join;
-import static org.testory.common.Checks.checkArgument;
 import static org.testory.common.Checks.checkNotNull;
 import static org.testory.common.Classes.tryWrap;
-import static org.testory.common.Collections.flip;
 import static org.testory.common.Collections.last;
 import static org.testory.common.Matchers.arrayOf;
 import static org.testory.common.Matchers.equalDeep;
 import static org.testory.common.Matchers.listOf;
 import static org.testory.common.Matchers.same;
-import static org.testory.common.Objects.areEqualDeep;
 import static org.testory.common.Objects.print;
 import static org.testory.proxy.Invocations.invocationOf;
 import static org.testory.util.Uniques.unique;
@@ -19,12 +16,13 @@ import static org.testory.util.Uniques.unique;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import org.testory.TestoryException;
 import org.testory.common.Matcher;
 import org.testory.common.Matchers;
 import org.testory.common.Matchers.ProxyMatcher;
-import org.testory.common.Nullable;
 import org.testory.proxy.Invocation;
 
 public class Any {
@@ -81,10 +79,45 @@ public class Any {
   }
 
   private static List<Boolean> solve(List<Any> anys, List<Object> arguments) {
-    List<Boolean> solution = trySolveEager(anys, arguments);
-    checkArgument(solution != null);
-    checkArgument(areEqualDeep(flip(solution), trySolveEager(flip(anys), flip(arguments))));
+    int firstAvailableArg = 0;
+    int firstAvailableAny = 0;
+    List<Boolean> solution = new ArrayList<Boolean>(arguments.size());
+
+    for (int i = 0; i < anys.size(); i++) {
+      Any any = anys.get(i);
+      int foundIndex = find(any, arguments);
+      if (foundIndex != -1) {
+        List<Any> anysSublist = anys.subList(firstAvailableAny, i);
+        List<Object> argumentsSublist = arguments.subList(firstAvailableArg, foundIndex);
+        solution.addAll(solveHidden(anysSublist, argumentsSublist));
+        solution.add(true);
+        firstAvailableArg = foundIndex + 1;
+        firstAvailableAny = i + 1;
+      }
+    }
+    List<Any> anysSublist = anys.subList(firstAvailableAny, anys.size());
+    List<Object> argumentsSublist = arguments.subList(firstAvailableArg, arguments.size());
+    solution.addAll(solveHidden(anysSublist, argumentsSublist));
     return solution;
+  }
+
+  private static int find(Any any, List<Object> arguments) {
+    for (int i = 0; i < arguments.size(); i++) {
+      if (any.mustBe(arguments.get(i))) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  private static Collection<? extends Boolean> solveHidden(List<Any> anys, List<Object> arguments) {
+    if (anys.size() == 0) {
+      return nCopies(arguments.size(), false);
+    }
+    if (anys.size() == arguments.size()) {
+      return nCopies(arguments.size(), true);
+    }
+    throw new TestoryException();
   }
 
   private static List<Object> unpackVarargs(List<?> packed) {
@@ -112,30 +145,6 @@ public class Any {
       matchers.add(matcher);
     }
     return matchers;
-  }
-
-  @Nullable
-  private static List<Boolean> trySolveEager(List<Any> anys, List<Object> arguments) {
-    List<Boolean> solution = new ArrayList<Boolean>(nCopies(arguments.size(), false));
-    int nextIndex = 0;
-    nextAny: for (Any any : anys) {
-      for (int i = nextIndex; i < arguments.size(); i++) {
-        if (any.mustBe(arguments.get(i))) {
-          solution.set(i, true);
-          nextIndex = i + 1;
-          continue nextAny;
-        }
-      }
-      for (int i = nextIndex; i < arguments.size(); i++) {
-        if (any.couldBe(arguments.get(i))) {
-          solution.set(i, true);
-          nextIndex = i + 1;
-          continue nextAny;
-        }
-      }
-      return null;
-    }
-    return solution;
   }
 
   private static Matcher asMatcher(final Object argument) {
